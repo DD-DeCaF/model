48444bc35bbc0abb6c5d0dbf8a055aa82dd3b8ef
Svetlana <galkina.s.a@gmail.com>
2018-03-02 11:30:30 +0100

    feat: add organometallic compounds to the list of salts

--- model/update_salts.py
+++ model/update_salts.py
@@ -1,14 +1,42 @@
+"""
+There could be potential problems with BIGG metabolites that differ in charge
+but have the same formula. Keep an eye on:
+fe2 and fe3
+mn2 and mn4
+cu2 and cu
+Both from the couple are added to the model, even if only one is the part
+of the medium chemical.
+"""
 import os
+import re
+import json
 import requests
 from urllib import request
 from multiprocessing import Pool
 from functools import partial
+from collections import defaultdict
 import xml.etree.ElementTree as ET
 
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-FILE_PATH = 'ftp://ftp.ebi.ac.uk/pub/databases/chebi/ontology/nightly/chebi.obo'
+FILE_PATH = 'ftp://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi.obo'
 N_PROCESSES = 20
+
+ID_MAPPER_API = os.environ['ID_MAPPER_API']
+
+
+def bigg_ids_items(items):
+    print("Start querying {} chemicals".format(len(items)))
+    return {k: bigg_ids(list(map(str, v))) for k, v in items}
+
+
+def bigg_ids(object_ids):
+    print("Call for {} chemical ids from id mapper".format(len(object_ids)))
+    query = json.dumps(
+        {'ids': object_ids, 'dbFrom': 'chebi', 'dbTo': 'bigg',
+         'type': 'Metabolite'})
+    r = requests.post(ID_MAPPER_API, data=query)
+    return r.json()['ids']
 
 
 def create_salts_mapping():
@@ -31,15 +59,87 @@ def create_salts_mapping():
                     [compounds_not_found[i::N_PROCESSES] for i in range(N_PROCESSES)]
             ):
                 smiles_to_chebi.update(result)
-        compounds_not_found = missing_compounds(salts, smiles_to_chebi)
+        missing_compounds(salts, smiles_to_chebi)
         salts = map_salts(smiles_to_chebi)
         print(f'{len(salts)} salts is mapped, '
               f'but one or more compounds are not present for '
               f'{len({k for k, v in salts.items() if [] in v})}')
-        # Iron trichloride hack until the proper solution is implemented
-        salts[30808] = [[29034], [13291]]
-        #
+
+        chebi_to_formula = generate_chebi_to_formula(chebi_to_inchi)
+        formula_to_chebi = {}
+        for k, v in chebi_to_formula.items():
+            formula_to_chebi[v] = formula_to_chebi.get(v, [])
+            formula_to_chebi[v].append(k)
+
+        chebi_to_formula_new_salts = {k: v for k, v in chebi_to_formula.items() if '.' in v and k not in salts}
+
+        metal_to_all_chebi = generate_metal_to_chebi(chebi_to_formula_new_salts, formula_to_chebi)
+        metal_to_chebi = metal_to_chebi_found_in_bigg(metal_to_all_chebi)
+
+        organometallic_compounds = get_organometallic_compounds(chebi_to_formula_new_salts, metal_to_chebi)
+
+        to_add = defaultdict(list)
+        for k, v in salts.items():
+            for array in v:
+                for element in array:
+                    if element in organometallic_compounds:
+                        to_add[k].extend(organometallic_compounds[element])
+        for k, v in to_add.items():
+            salts[k].extend(v)
+        salts.update(organometallic_compounds)
+        print(f'With {len(organometallic_compounds)} organometallic compounds '
+              f'the number of salts is {len(salts)}')
+
         save_salts(salts)
+
+
+def get_organometallic_compounds(chebi_to_formula_new_salts,
+                                 metal_to_chebi):
+    organometallic_compounds = {}
+    for k, v in chebi_to_formula_new_salts.items():
+        result = []
+        for element in v.split('.'):
+            element = element.lstrip('0123456789')
+            result.append(metal_to_chebi.get(element, []))
+        if any(result):
+            organometallic_compounds[k] = result
+    return organometallic_compounds
+
+
+def metal_to_chebi_found_in_bigg(metal_to_all_chebi):
+    metal_to_bigg = {}
+    with Pool(processes=N_PROCESSES) as pool:
+        for result in pool.imap_unordered(
+                bigg_ids_items,
+                [list(metal_to_all_chebi.items())[i::N_PROCESSES] for i
+                 in range(N_PROCESSES)]
+        ):
+            metal_to_bigg.update(result)
+    metal_to_chebi = {k: list(map(int, v.keys())) for k, v in
+                      metal_to_bigg.items() if v}
+    return metal_to_chebi
+
+
+def generate_metal_to_chebi(chebi_to_formula_new_salts, formula_to_chebi):
+    chebi_to_shortest_splits = {}
+    all_shortest = []
+    for k, v in chebi_to_formula_new_salts.items():
+        shortests = [i.lstrip('0123456789') for i in v.split('.') if
+                     len(i.lstrip('0123456789')) <= 3]
+        all_shortest.extend(shortests)
+        chebi_to_shortest_splits[k] = shortests
+    all_shortest = set(all_shortest)
+    metal_to_all_chebi = {i: formula_to_chebi[i] for i in all_shortest
+                          if i in formula_to_chebi}
+    return metal_to_all_chebi
+
+
+def generate_chebi_to_formula(chebi_to_inchi):
+    chebi_to_formula = {k: re.match(r'InChI=1S\/([^/]+)\/', v) for k, v in
+                        chebi_to_inchi.items()}
+    chebi_to_formula = {k: v.group(1) for k, v in chebi_to_formula.items()
+                        if v}
+    return chebi_to_formula
 
 
 def save_salts(salts_mapping):
