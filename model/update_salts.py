156c47e9711ccbc16c08d5600602c6448b3bb815
Ali Kaafarani <ali@kvikshaug.no>
2018-03-12 17:27:23 +0100

    style: newline after license

--- model/update_salts.py
+++ model/update_salts.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 There could be potential problems with BIGG metabolites that differ in charge
 but have the same formula. Keep an eye on:
