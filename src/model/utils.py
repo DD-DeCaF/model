456edc1178ba02629165cad44854671bceee5ef2
Ali Kaafarani <ali@kvikshaug.no>
2018-07-12 09:21:06 +0200

    feat: add Singleton type

--- src/model/utils.py
+++ src/model/utils.py
@@ -21,6 +21,15 @@ from functools import wraps
 logger = logging.getLogger(__name__)
 
 
+class Singleton(type):
+    """Implement the singleton pattern by using this type as metaclass"""
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
 def timing(f):
     @wraps(f)
     def wrap(*args, **kwargs):
