e62ad4e67dce305eab24987d05b10775bb60f7c4
Ali Kaafarani <ali@kvikshaug.no>
2018-08-14 09:15:08 +0200

    refactor: don't use model context manager during simulations

--- src/model/simulations.py
+++ src/model/simulations.py
@@ -36,48 +36,47 @@ def simulate(model, method, objective_id, objective_direction, tmy_objectives):
     if method not in METHODS:
         raise ValueError(f"Unsupported simulation method '{method}'")
 
-    with model:
-        if objective_id:
-            model.objective = model.reactions.get_by_id(objective_id)
-        if objective_direction:
-            model.objective.direction = objective_direction
+    if objective_id:
+        model.objective = model.reactions.get_by_id(objective_id)
+    if objective_direction:
+        model.objective.direction = objective_direction
 
-        try:
-            logger.info(f"Simulating model {model.id} with {method}")
-            if method == 'fba':
-                solution = model.optimize()
-            elif method == 'pfba':
-                solution = pfba(model)
-            elif method == 'fva':
-                # FIXME: accept list of relevant fva reactions to calculate
-                solution = flux_variability_analysis(model)
-            elif method == 'pfba-fva':
-                # FIXME: accept list of relevant fva reactions to calculate
-                solution = flux_variability_analysis(model, fraction_of_optimum=1, pfba_factor=1.05)
-        except OptimizationError as error:
-            logger.info(f"Optimization Error: {error}")
-            flux_distribution = {}
-            growth_rate = 0.0
+    try:
+        logger.info(f"Simulating model {model.id} with {method}")
+        if method == 'fba':
+            solution = model.optimize()
+        elif method == 'pfba':
+            solution = pfba(model)
+        elif method == 'fva':
+            # FIXME: accept list of relevant fva reactions to calculate
+            solution = flux_variability_analysis(model)
+        elif method == 'pfba-fva':
+            # FIXME: accept list of relevant fva reactions to calculate
+            solution = flux_variability_analysis(model, fraction_of_optimum=1, pfba_factor=1.05)
+    except OptimizationError as error:
+        logger.info(f"Optimization Error: {error}")
+        flux_distribution = {}
+        growth_rate = 0.0
 
-            # For non-fva methods, do return the measured fluxes despite infesability
-            if method not in ('fva', 'pfba-fva'):
-                changes = model.notes['changes']
-                if 'measured' in changes:
-                    ids_measured_reactions = set(rxn['id'] for rxn in changes['measured']['reactions'])
-                    flux_distribution = {
-                        rxn.id: (rxn.upper_bound + rxn.lower_bound) / 2
-                        for rxn in model.reactions if rxn.id in ids_measured_reactions
-                    }
-        else:
-            logger.info(f"Simulation completed successfully")
-            if method in ('fba', 'pfba'):
-                flux_distribution = solution.fluxes.to_dict()
-                growth_rate = flux_distribution[storage.get(model.id).growth_rate_reaction]
-            elif method in ('fva', 'pfba-fva'):
-                df = solution.rename(index=str, columns={'maximum': 'upper_bound', 'minimum': 'lower_bound'})
-                for key in ['lower_bound', 'upper_bound']:
-                    df[key] = df[key].astype('float')
-                flux_distribution = df.T.to_dict()
-                growth_rate = flux_distribution[storage.get(model.id).growth_rate_reaction]['upper_bound']
+        # For non-fva methods, do return the measured fluxes despite infesability
+        if method not in ('fva', 'pfba-fva'):
+            changes = model.notes['changes']
+            if 'measured' in changes:
+                ids_measured_reactions = set(rxn['id'] for rxn in changes['measured']['reactions'])
+                flux_distribution = {
+                    rxn.id: (rxn.upper_bound + rxn.lower_bound) / 2
+                    for rxn in model.reactions if rxn.id in ids_measured_reactions
+                }
+    else:
+        logger.info(f"Simulation completed successfully")
+        if method in ('fba', 'pfba'):
+            flux_distribution = solution.fluxes.to_dict()
+            growth_rate = flux_distribution[storage.get(model.id).growth_rate_reaction]
+        elif method in ('fva', 'pfba-fva'):
+            df = solution.rename(index=str, columns={'maximum': 'upper_bound', 'minimum': 'lower_bound'})
+            for key in ['lower_bound', 'upper_bound']:
+                df[key] = df[key].astype('float')
+            flux_distribution = df.T.to_dict()
+            growth_rate = flux_distribution[storage.get(model.id).growth_rate_reaction]['upper_bound']
 
     return flux_distribution, growth_rate
